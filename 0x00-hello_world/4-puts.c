368805434e0410ef26ba2f1d826e297944126b53
daprinco <daprinco@gmail.com>
2022-10-13 09:17:15 -0700

    put file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: function declaration
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
