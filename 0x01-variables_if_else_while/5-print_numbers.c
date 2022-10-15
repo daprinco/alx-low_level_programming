aca83bc0c3c07720a5bafcdc919cad499524217a
daprinco <daprinco@gmail.com>
2022-10-15 11:40:26 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: function declaration
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
