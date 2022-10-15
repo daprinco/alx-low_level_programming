14173aa7a32d1d6f3f4a27cb23afd69cfa264dcf
daprinco <daprinco@gmail.com>
2022-10-15 16:52:38 -0700

    reverse lowercase alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: function declaration
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}
+
