8bc3ab742a2c25244a048f380252bc582a4742b0
daprinco <daprinco@gmail.com>
2022-10-15 05:10:34 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: function declaration
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
+
