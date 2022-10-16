cdbea53e783036d8595db1b784b7d5a2f19732ce
daprinco <daprinco@gmail.com>
2022-10-16 05:57:28 -0700

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: function declaration
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 9)
+		{	putchar(48 + i);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(48 + i);
+		}
+		i++;
+	}
+
+	return (0);
+}
+
