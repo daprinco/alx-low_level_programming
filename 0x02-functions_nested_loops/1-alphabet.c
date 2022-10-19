926262d32b8033573e47e14737044a1cc85b61c7
daprinco <daprinco@gmail.com>
2022-10-18 21:25:39 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char i = a;
+
+	while (i <= z)
+	{
+		_putchar(x);
+		x++;
+	}
+
+	_putchar('\n');
+
+}
