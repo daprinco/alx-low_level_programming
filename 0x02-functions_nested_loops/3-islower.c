4a1c5a9790eb90547246ace49c91b5ad2839df1e
daprinco <daprinco@gmail.com>
2022-10-18 22:26:44 -0700

    is lower_case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - returns lower case alph
+ *
+ * @c: is an input to function
+ *
+ * Return:1 if lower and 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	else
+		return (0);
+
+}
