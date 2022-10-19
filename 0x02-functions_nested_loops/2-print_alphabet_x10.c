143d82fe2efff2ec9100b8f30774efd5f9931361
daprinco <daprinco@gmail.com>
2022-10-18 21:58:45 -0700

    10x lowercase alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10x alpha in lower case
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	char i = 'a';
+
+	for (j = 0; j < 10; j++)
+	{
+
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+
+		_putchar('\n');
+	}
+
+}
