8b3cc3378465f122fea4e1d2be086befb576dff6
daprinco <daprinco@gmail.com>
2022-10-18 22:50:44 -0700

    positive negative int

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - returns lower case alph
+ *
+ * @c: is an input to function
+ *
+ * Return:1 if lower and 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z') || (c >= 'A'
+	c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+
+}
