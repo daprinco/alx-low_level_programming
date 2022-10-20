bb45defd43cc9d18b383f8e48850a43950774839
daprinco <daprinco@gmail.com>
2022-10-19 19:34:21 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+
+/**
+ * add - function to add 2 integers
+ *
+ * @a: first integer input
+ *
+ * @b: second integer input
+ *
+ * Return: the sum of the 2 integers
+ */
+
+int add(int a, int b)
+{
+	int total;
+
+	total = a + b;
+
+	return (total);
+
+}
