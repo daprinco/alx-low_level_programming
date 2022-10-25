ea703e22348c6c2636397dcacd13f734fa2dd5f0
daprinco <daprinco@gmail.com>
2022-10-25 03:58:52 -0700

    task 100_1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	while (!('0' < = *s && *s < = '9') && *s != '\0')
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '-')
 			sign *= -1;
@@ -19,7 +19,7 @@ int _atoi(char *s)
 			sign *= +1;
 		s++;
 	}
-	while ('0' < = *s && *s < = '9' && *s != '\0')
+	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
 		num = (num * 10) + (*s - '0');
 		s++;
