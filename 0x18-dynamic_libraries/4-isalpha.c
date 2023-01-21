b162c125addefbcd42869e44db90d171ad83aaa6
daprinco <daprinco@gmail.com>
2023-01-20 20:31:56 -0800

    update

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - returns lower case alph
+ *
+ * @c: is an input to function
+ *
+ * Return:1 if lower and 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+
+	else
+		return (0);
+
+}
